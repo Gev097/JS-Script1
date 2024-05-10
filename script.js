2a328f19659d3f3532705cf89e9bba2585e1b98d
Gevorg <gev.grigoryan097@gmail.com>
2024-05-10 18:16:41 +0400

    I add JS Script

---
+++ script.js
@@ -0,0 +1,46 @@
+// for (let i = 2; i <= 30; i +=2) {
+//     console.log(i);
+    
+// }
+
+// let x = 0;
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % 17 === 0) {
+//         x++;
+//     }
+// }
+// console.log(x);
+
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 19 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let x = 0;
+// let result;
+
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % 7 === 0) {
+//         x++;
+//         if (x === 123) {
+//             result = i;
+//         }
+//     }
+// }
+
+// console.log(result);
+
+
+// let x = 1
+
+// for(let i = 6; i > 0; i--){
+//   x = x * i 
+// }
+
+// console.log(x)
+
+
+
